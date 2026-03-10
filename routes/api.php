681ce278492ba3f6d4150a213bd4a713d2ffba77
Kevin Ramos <ramosgarciakevinesteban2007@gmail.com>
2026-03-10 18:55:25 -0500

    Implementacion controlador, validaciones y rutas

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,11 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('products', ProductController::class);
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{product}', [ProductController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('products', [ProductController::class, 'store']);
+    Route::patch('products/{product}', [ProductController::class, 'update']);
+    Route::delete('products/{product}', [ProductController::class, 'destroy']);
+});
