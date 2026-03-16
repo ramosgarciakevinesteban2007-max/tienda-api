6b427f8462768a37fd72cde015febb9da4779617
Darx-Campos <darwinmauriciocampos0@gmail.com>
2026-03-16 11:37:39 -0500

    main

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -7,13 +7,13 @@
 @foreach($products as $product) 
     <ul>
         <li>{{ $product->name }}</li>
-        <button onclick="agrgarAlcarrito({{ $product }})">Añadir al carrito</button>
+        <button onclick="agregarAlcarrito({{ $product }})">Añadir al carrito</button>
     </ul>    
 @endforeach 
 
 <script>
     let carrito = []
-    function agrgarAlcarrito(product){
+    function agregarAlcarrito(product){
         let posicion = carrito.findIndex(item => item.id === product.id)
         if(posicion !== -1){
             carrito[posicion].cantidad++
@@ -22,13 +22,14 @@
             carrito.push(product)
         }
         console.log(carrito)
-
+        mostrarCarrito();
     } 
     
     function mostrarCarrito(){
-        Let divcarrito = document.getElementById('carrito')
+        let divcarrito = document.getElementById('carrito')
+        divcarrito.innerHTML = ''
         carrito.map(item => {
-            divcarrito.innerHTML += `<p>${item.name} - ${item.cantidad}</p>`
-        })
+            divcarrito.innerHTML += `<p>${item.name} : ${item.cantidad}</p>`
+        });
     }
 </script>    
